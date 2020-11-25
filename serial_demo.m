92052584ce2cb1b0ceb6cbe702c2668ae68b4f93
Jens Bongartz <32654669+jens-bongartz@users.noreply.github.com>
2020-11-25 12:35:17 +0100

    Add files via upload

---
+++ serial_demo.m
@@ -0,0 +1,19 @@
+pkg load instrument-control;
+
+serial_01 = serialport("COM9",115200);
+
+flush(serial_01);
+
+do
+   bytesavailable = get(serial_01,"numbytesavailable");
+   
+   if (bytesavailable >0)
+     inSerial = fread(serial_01);
+     inChar = char(inSerial)
+   endif
+
+until(kbhit(1) == 'x');
+
+clear serial_01;
+
+
